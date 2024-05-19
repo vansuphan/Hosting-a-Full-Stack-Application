4414253287d8b885480387f03df23e615b841c68
vansuphan <vansuphan.kma@gmail.com>
2024-05-19 14:23:04 +0700

    add elasticbeántalk

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -6,6 +6,7 @@ dotenv.config();
 // - AWS_SECRET_ACCESS_KEY
 // Are Also needed
 
+
 export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
   password: process.env.POSTGRES_PASSWORD,
